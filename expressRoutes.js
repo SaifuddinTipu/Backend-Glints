0280cdb5d9e9f5d612cdf0576bf45719ff1972be
SaifuddinTipu <tipu.luc.my@gmail.com>
2022-02-06 11:05:42 +0800

    update

--- expressRoutes.js
+++ expressRoutes.js
@@ -1,12 +1,12 @@
 const users = require("./routes/users");
 const restaurants = require("./routes/restaurants");
-
+const url = 'https://backend-glints.herokuapp.com';
 const expressRoutes = async (appExpress) => {
   appExpress.use(function (req, res, next) {
     next();
   });
-  appExpress.use("/api/users", users);
-  appExpress.use("/api/restaurants", restaurants);
+  appExpress.use(`${url}/api/users`, users);
+  appExpress.use(`${url}/api/restaurants`, restaurants);
 };
 
 module.exports = expressRoutes;
