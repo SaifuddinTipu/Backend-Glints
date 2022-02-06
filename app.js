e6f6e5bd1eff82b9a2405a02b97b02ed0f3f1049
SaifuddinTipu <tipu.luc.my@gmail.com>
2022-02-06 10:34:39 +0800

    update

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const onListening = () => {
   console.log("Listening on" + bind);
 };
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(Process.env.PORT || "3000");
 
 const server = http.createServer(appExpress);
 
